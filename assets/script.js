3a6d9eb2ed66038029d0d89ac6fe7852c5dbf1f7
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2021-12-02 12:53:32 -0700

    s/form submit handler and breweries api

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,29 @@
 console.log('js linked successfully')
+
+var currentAddress = document.querySelector("form-control")
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    console.log(event);
+    var startingPoint = currentAddress.value.trim();
+    console.log(startingPoint);
+}
+
+var getBreweries = function (breweryName) {
+    var listBreweriesApi = "https://api.openbrewerydb.org/breweries?by_city=salt_lake_city&by_type=micro&per_page=15" + breweryName + "&appid="
+    // fetch(listBreweriesApi);
+    // console.log(data);
+    // .then(function(response) {
+    //     if(response.ok) {
+    //         response.json().then(function(data) {
+    //             console.log(data);
+    //             breweryName.innerText = 
+    //         }
+    //         )
+    //     }
+    // }
+    // )
+};
+// create buttons for breweries
+// save breweries to local storage
+// current position function
