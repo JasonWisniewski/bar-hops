7ced98637cd6eb6d04feeb60fdf92396c8e74d49
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2021-12-02 19:51:04 -0700

    s+j+n we all worked on the brewery api and submit handler function

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,34 @@
 console.log('js linked successfully')
 
-var currentAddress = document.querySelector("form-control")
+var currentAddress = document.querySelector("#userAddress")
+var searchButton = document.querySelector("#search-button")
+
+
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
     console.log(event);
-    var startingPoint = currentAddress.value.trim();
-    console.log(startingPoint);
+    var startingPoint = currentAddress.value
+    console.log(startingPoint)
 }
 
 var getBreweries = function (breweryName) {
-    var listBreweriesApi = "https://api.openbrewerydb.org/breweries?by_city=salt_lake_city&by_type=micro&per_page=15" + breweryName + "&appid="
-    // fetch(listBreweriesApi);
-    // console.log(data);
-    // .then(function(response) {
-    //     if(response.ok) {
-    //         response.json().then(function(data) {
-    //             console.log(data);
-    //             breweryName.innerText = 
-    //         }
-    //         )
-    //     }
-    // }
-    // )
+    var listBreweriesApi = "https://api.openbrewerydb.org/breweries?by_city=salt_lake_city&sort=name:desc"
+    fetch(listBreweriesApi)
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                // breweryName.innerText = 
+            }
+            )
+        }
+    }
+    )
 };
+getBreweries();
 // create buttons for breweries
 // save breweries to local storage
 // current position function
+
+searchButton.onclick = formSubmitHandler;
